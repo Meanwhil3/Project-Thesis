af7eb12b94435143f79124e4de700172ab44842d
EyeOnwara <109953143+EyeOnwara@users.noreply.github.com>
2025-12-02 15:35:57 +0700

    update treeinfo

---
+++ app/tree/[id]/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+const TreeInfo = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+        
+    </div>
+  );
+};
+
+export default TreeInfo;
